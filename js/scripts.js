000ebef1537399448019c1df2f2f8dfe168fa09d
Stefano <tepo@hotmail.it>
2021-04-22 17:04:55 +0200

    Inseriti dati con vuejs per sezione pizza e workers

--- js/scripts.js
+++ js/scripts.js
@@ -80,7 +80,116 @@ var app = new Vue(
           image: ""
         },
       ],
+      workers: [
+        {
+          name: "Peter Anderson",
+          job: "Head Chef",
+          image: "img/h1-team-1a-700x700.jpg",
+          socialLinks: {
+            instagram: "https://www.instagram.com",
+            twitter: "https://www.twitter.com",
+            facebook: "https://www.facebook.com"
+          }
+        },
+        {
+          name: "Sharon Hayes",
+          job: "Executive Chef",
+          image: "img/h1-team-2a.jpg",
+          socialLinks: {
+            instagram: "https://www.instagram.com",
+            twitter: "https://www.twitter.com",
+            facebook: "https://www.facebook.com"
+          }
+        },
+        {
+          name: "Frank Bailey",
+          job: "Kitchen Porter",
+          image: "img/h1-team-4a.jpg",
+          socialLinks: {
+            instagram: "https://www.instagram.com",
+            twitter: "https://www.twitter.com",
+            facebook: "https://www.facebook.com"
+          }
+        },
+        {
+          name: "Carol Simpson",
+          job: "Sous Chef",
+          image: "img/h1-team-3a.jpg",
+          socialLinks: {
+            instagram: "https://www.instagram.com",
+            twitter: "https://www.twitter.com",
+            facebook: "https://www.facebook.com"
+          }
+        },
+      ],
+      pizzas: [
+        {
+          name: "Bismarck",
+          image: "img/h3-product-img-1a-100x100.png",
+          price: "$30.00",
+          discount: {
+            applied: true,
+            oldPrice: "$55.00"
+          },
+          first: true,
+          last: false
+        },
+        {
+          name: "Fiori di zucca",
+          image: "img/h3-product-img-2a-150x150.png",
+          price: "$7.00 - $50.00",
+          discount: {
+            applied: false,
+            oldPrice: null
+          },
+          first: false,
+          last: false
+        },
+        {
+          name: "Valdostana",
+          image: "img/h3-product-img-6a-100x100.png",
+          price: "$55.00",
+          discount: {
+            applied: false,
+            oldPrice: null
+          },
+          first: false,
+          last: false
+        },
+        {
+          name: "Pizza tartufata",
+          image: "img/h3-product-img-4a-150x150.png",
+          price: "$45.00",
+          discount: {
+            applied: false,
+            oldPrice: null
+          },
+          first: false,
+          last: false
+        },
+        {
+          name: "Francescana",
+          image: "img/h3-product-img-5a-150x150.png",
+          price: "$25.00",
+          discount: {
+            applied: false,
+            oldPrice: null
+          },
+          first: false,
+          last: false
+        },
+        {
+          name: "Campagnola",
+          image: "img/h3-product-img-3a-150x150.png",
+          price: "$50.00 - $95.00",
+          discount: {
+            applied: false,
+            oldPrice: null
+          },
+          first: false,
+          last: true
+        },
+      ]
     },
-
   }
 );
